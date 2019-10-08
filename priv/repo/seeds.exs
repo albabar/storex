48a94ab5b3fcffa1b2bb8fe7167b0a302f4ab607
Babar Al-Amin <knock@babar.im>
2019-10-08 21:06:23 +0700

    Add seeds for books

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,33 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+books = [
+  %{
+    "title" => "Eating Well",
+    "description" => "Healthy recipes for busy lifes",
+    "price" => "39.00",
+    "image_url" => "http://www.phoenixforrailsdevelopers.com/books/1.png"
+   },
+  %{
+    "title" => "Lovely flowers to you",
+    "description" => "The story about how flowers changed the world",
+    "price" => "28.00",
+    "image_url" => "http://www.phoenixforrailsdevelopers.com/books/2.png"
+  },
+  %{
+    "title" => "My dog is my family hero",
+    "description" => "Everything we have, we own to our dog",
+    "price" => "35.00",
+    "image_url" => "http://www.phoenixforrailsdevelopers.com/books/3.png"
+  },
+  %{
+    "title" => "Just build it",
+    "description" => "How to build a building",
+    "price" => "23.00",
+    "image_url" => "http://www.phoenixforrailsdevelopers.com/books/4.png"
+  }
+]
+
+alias Storex.Store.Book
+books |> Enum.each(fn book -> Storex.Repo.insert!(Book.changeset(%Book{}, book)) end)
